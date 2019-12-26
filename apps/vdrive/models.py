eb402c336cb855b92e8056e23bb2ad2af8d1237a
Illya Smirnov <illya.smirnov@akvelon.com>
2019-12-26 20:05:20 +0200

    Fixing mistakes

--- apps/vdrive/models.py
+++ apps/vdrive/models.py
@@ -1,55 +1,35 @@
 from django.db import models
 
+
 from django.template.defaultfilters import slugify
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
 class Processing(models.Model):
-    id = models.AutoField(primary_key=True)
-    date = models.DateTimeField(
-        verbose_name="creation date",
-        auto_now_add=True, null=True
-    )
-    source_type = models.URLField(
-        verbose_name="source of the file",
-        max_length=250, unique=True
-    )
+
+    class Type(models.TextChoices):
+        GDRIVE = 'GD', _('Gdrive type')
+        GPHOTOS = 'GPH', _('Gphotos type')
+
+    date = models.DateTimeField(verbose_name=_("creation date"), auto_now_add=True, null=True)
+    source_type = models.CharField(choices=Type.choices, verbose_name=_("source of the file"), max_length=250, unique=True)
 
 
-class VideoProc(models.Model):
+class VideoProcessing(models.Model):
 
-    Waiting = 'WT'
-    InProgress = 'InPr'
-    Error_Message = 'ErrorMes'
-    Successful_Upload = 'SuccUp'
-    STATUS_OF_VIDEO_UPLOADING = [
-        (Waiting, 'Waiting to upload'),
-        (InProgress, 'In progress of uploading'),
-        (Error_Message, 'Error message'),
-        (Successful_Upload, 'Successfully Uploaded'),
-    ]
+    class Status(models.TextChoices):
+        DOWNLOAD = 'DW', _('Download')
+        WAITING = 'WT', _('Waiting to upload')
+        IN_PROGRESS = 'IN_PR', _('In progress of uploading')
+        ERROR_MESSAGE = 'ERROR_MESS', _('Error message')
+        SUCCESSFUL_UPLOAD = 'SUCC_UP', _('Successfully Uploaded')
 
-    proc_id = models.ForeignKey('vdrive.Processing', on_delete=models.CASCADE)
-    source_id = models.AutoField(
-        verbose_name=" id of video in the source",
-        primary_key=True,
-        unique=True
-        )
-    status_video = models.CharField(
-        max_length=8,
-        choices=STATUS_OF_VIDEO_UPLOADING,
-        default=Waiting,
-    )
-    error_message_video = models.CharField(default="Error. Something went wrong", max_length=250)
-    youtube_id = models.CharField(
-        verbose_name=" id of youtube uploaded video",
-        max_length=250,
-        unique=True,
-    )
-    youtube_link = models.URLField(
-        verbose_name="link of the video from youtube",
-        max_length=250,
-        unique=True
-    )
+    processing = models.ForeignKey('vdrive.Processing', on_delete=models.CASCADE)
+    source_id = models.CharField(verbose_name=_(" id of video in the source"),max_length=250, primary_key=True, unique=True,)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.DOWNLOAD,)
+    error_message_video = models.TextField(max_length=500, blank=True)
+    youtube_id = models.CharField(verbose_name=_("id of youtube uploaded video"), max_length=250, unique=True,)
+    youtube_link = models.URLField(verbose_name=_("link of the video from youtube"), max_length=250, unique=True)
 
 
