0c5c89239f522508f99791eb34842398f68ae58a
Bohdan <bohdan.shkliar@akvelon.com>
2019-12-26 20:59:17 +0200

    added downloading from Gd to file

--- apps/vdrive/views.py
+++ apps/vdrive/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from django.views.generic import TemplateView
+from apiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
 
 
 class GDriveListView(LoginRequiredMixin, TemplateView):
@@ -12,10 +14,14 @@ class GDriveListView(LoginRequiredMixin, TemplateView):
         social = user.social_auth.get(provider='google-oauth2')
         creds = Credentials(social.extra_data['access_token'])
         drive = build('drive', 'v3', credentials=creds)
-        files_data = drive.files().list().execute()
-        print(files_data)
-        return files_data['files']
-
+        request = drive.files().get_media(fileId='1FlF-ko_30BcGt_bhmO1jFxgfU-voHzLH')
+        # downloading to 1.mp4 file
+        with open("1.mp4", 'wb+') as fd:
+            downloader = MediaIoBaseDownload(fd, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print("Download %d%%." % int(status.progress() * 100))
     def get_context_data(self):
         return {
             'files': self.get_files_list()
